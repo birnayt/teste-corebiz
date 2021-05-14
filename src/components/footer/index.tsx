2c36108c66d276a9653ee4ec46d87e420cf1fee1
LucasFr <lucasfariasrodrigues25@gmail.com>
2021-05-13 21:53:58 -0300

    Finish footer desktop and mobile

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import style from './footer.module.scss';
+
+import iconCoreBiz from '../../assets/imgs/logo-footer.png';
+import iconVtex from '../../assets/imgs/icon-vtex.png';
+import iconEmail from '../../assets/imgs/icon-email.png';
+import iconContact from '../../assets/imgs/icon-contact.png';
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className={style.localization}>
+        <h2 className={style.localizationTitle}>Localização</h2>
+        <ul className={style.companyInfos}>
+          <li>Avenida Andrômeda, 2000. Bloco 6 e 8</li>
+          <li>Alphaville SP</li>
+          <li><a href="mailto:brasil@corebiz.ag" title="E-mail CoreBiz">brasil@corebiz.ag</a></li>
+          <li><a href="tel:+551130901039" title="Telephone CoreBiz">+55 11 3090 1039</a></li>
+        </ul>
+      </div>
+
+      <div className={style.contact}>
+        <button className={style.emailContact}>
+          <img src={iconEmail} />
+          <p>Entre em contato</p>
+        </button>
+
+        <button className={style.talkToUs}>
+          <img src={iconContact} />
+          <p>Fale com o nosso consultor online</p>
+        </button>
+      </div>
+
+      <div className={style.signature}>
+        <div className={style.signatureDiv}>
+          Covered by
+          <img src={iconCoreBiz} />
+        </div>
+        <div className={style.signatureDiv}>
+          Powered By
+          <img src={iconVtex} className={style.iconVtex} />
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable camelcase */
 import React from 'react';
 import Header from '../../components/header';
+import Footer from '../../components/footer';
+
 
 const HomePage = () => {
-
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
